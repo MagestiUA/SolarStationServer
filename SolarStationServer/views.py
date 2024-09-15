3b40120dd59c7dea6f2e63eab2e2d1958d7d3084
MagestiUA <nikolaiq3@gmail.com>
2024-09-15 10:43:18 +0300

    зміна коефініенту вироблення панелей,з урахуванням розбіжності встановленої потужності на кожному інверторі

--- SolarStationServer/views.py
+++ SolarStationServer/views.py
@@ -51,8 +51,8 @@ def data_collector(request):
                     # batt_power=avg_inv_data['Batt power']*2,
                     # batt_current=avg_inv_data['Batt current']*2,
                     pv_voltage=avg_inv_data['PV voltage'],
-                    charger_current=avg_inv_data['Charger current']*2,
-                    charger_power=avg_inv_data['Charger power']*2,
+                    charger_current=avg_inv_data['Charger current']*1.9,
+                    charger_power=avg_inv_data['Charger power']*1.9,
                 )
                 inverter_data.save()
             
