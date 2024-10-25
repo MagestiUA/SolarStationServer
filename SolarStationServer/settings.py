e63c822fc399c0b8be483a5606b2b795d8328ece
MagestiUA <nikolaiq3@gmail.com>
2024-10-25 18:57:25 +0300

    fix

--- SolarStationServer/settings.py
+++ SolarStationServer/settings.py
@@ -214,9 +214,17 @@ LOGGING = {
 }
 SOCIALACCOUNT_PROVIDERS = {
     'google': {
-        'SCOPE': ['profile', 'email'],
-        'AUTH_PARAMS': {'access_type': 'online'},
-        'CLIENT_ID': os.getenv('GOOGLE_CLIENT_ID'),
-        'SECRET': os.getenv('GOOGLE_CLIENT_SECRET'),
+        'APP': {
+            'client_id': '<GOOGLE_CLIENT_ID>',
+            'secret': '<GOOGLE_CLIENT_SECRET>',
+            'key': ''
+        },
+        'SCOPE': [
+            'profile',
+            'email',
+        ],
+        'AUTH_PARAMS': {
+            'access_type': 'online',
+        },
     }
 }
